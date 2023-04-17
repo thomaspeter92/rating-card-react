65556fb7aad47fe9f8286c19e0f7f612603aa126
Thomas Buckley <thomasbuckley@Thomass-MacBook-Pro-2.local>
2023-04-17 15:45:06 +0900

    starter project

--- src/App.jsx
+++ src/App.jsx
@@ -13,33 +13,18 @@ function App() {
     <div className="app">
       <div className="container">
         {page === 1 ? 
-        <>
-          <div className="iconContainer">
-          <img src={StarIcon} alt="star icon" />
+          <div className="page1">
+              {nums.map((num,i) => (
+                <div onClick={() => {setSelectedNum(num)}} className={`num ${selectedNum === num ? 'selected' : null}`}>{num}</div>
+              ))
+              }
           </div>
-          <p className="bigText">How did we do?</p>
-          <p className="smallText">Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!</p>
-          <div className="numBox">
-          {nums.map((num,i) => (
-            <div onClick={() => {setSelectedNum(num)}} className={`num ${selectedNum === num ? 'selected' : null}`}>{num}</div>
-          ))
-          }
-          </div>
-          <button onClick={() => setPage(2)}>SUBMIT</button>
-        </>
-       : 
-
+       : null }
+       {page === 2 ?
         <div className="page2">
-        <img src={PhoneSVG} />
-        <div className="userSelected">
-          <p>You Selected {selectedNum} our of 5</p>
+        
         </div>
-        <p className="bigText">Thank you!</p>
-        <p className="smallText">  We appreciate you taking the time to give a rating. If you ever need more support, 
-  don’t hesitate to get in touch!</p>
-       </div>
-       }
-       
+        : null}
       </div>
     </div>
   )
